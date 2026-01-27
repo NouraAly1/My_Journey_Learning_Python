6f877130114a824f38f5413a52b45e177d2e5dc3
nouraaly1 <nouraaly1@users.noreply.github.com>
2026-01-27 14:05:45 -0500

    Stop tracking codezilla_restaurant_cal2.py

--- codezilla_restaurant_cal2.py
+++
@@ -1,9 +0,0 @@
-# get order cost from user 
-order_cost = float(input("Enter order cost: "))
-
-# creat a function to add the fees
-def total_cost():
-    return order_cost + order_cost * 0.10
-
-# print the total cost
-print ("The total cost is : ", total_cost())
