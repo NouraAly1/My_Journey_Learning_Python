4f1695b09077261c4e37d9b9aa1c6f9e10d8fc11
nouraaly1 <nouraaly1@users.noreply.github.com>
2026-01-27 12:02:44 -0500

    Stop tracking codedictionaries.py

--- codedictionaries.py
+++
@@ -1,8 +0,0 @@
-month_name = {
-    "0" : "april, the fourth month",
-    "may" : "mayo",
-    "aug" : "august",
-    "sep" : "september"
-}
-print(month_name["0"])
-print(month_name.get("sep", "value does not exist"))
