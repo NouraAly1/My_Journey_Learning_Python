ca033ae08bc65c1c503b82f1f9ee30423afc4f70
nouraaly1 <nouraaly1@users.noreply.github.com>
2026-01-27 14:47:06 -0500

    Rename company_id_email.py to employee_credentials_generator.py

--- employee_credentials_generator.py
+++ employee_credentials_generator.py
@@ -1,18 +1,22 @@
 # id = company name + last letters from name + DOB
 # email = take off from the '@' till the end and add '@apple.com
 
-#get info from user
+# get info from user
 company_name = 'apple'
 name = input("Enter your name: ").strip()
-DOB = input("Enter your date of birth: ")
+DOB = input("Enter your date of birth: ").strip()
 email = input("Enter your email: ").strip()
 
 # creating the id with slicing the last 3 letters
 last_three = name[-3:]
-id = company_name + last_three + DOB
-print(f'youe id is : {id}')
+employee_id = company_name + last_three + DOB
+print(f'your id is : {employee_id}')
 
 # edit user's email
-index_of_at = email.index("@")
+try:
+    index_of_at = email.index("@")
+except ValueError:
+    print("Error: Email must contain '@'")
+    exit()
 new_email = email[:index_of_at] + '@apple.com'
 print(f'your new email is : {new_email}')
